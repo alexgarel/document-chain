cbd7c3e762fad8e12735ecedfeae7f2a64473699
Alex Garel <alex.garel@gmail.com>
2011-05-31 00:46:42 +0200

    added user and password option

--- src/document_chain/http_submitter.py
+++ src/document_chain/http_submitter.py
@@ -10,13 +10,15 @@ try:
 
         name = 'HTTP file submitter'
 
-        def run_action(self, url, file_path, field_name='file',
-                        cookie_data=None, **kwargs):
+        def run_action(self, url, file_path, user=None, password=None,
+                       field_name='file', **kwargs):
             """Submit file to url
             """
             form = [(field_name, (pycurl.FORM_FILE, file_path))]
             curl = pycurl.Curl()
             curl.setopt(pycurl.URL, url)
+            if user is not None:
+                curl.setopt(curl.USERPWD, '%s:%s' % (user, password))
             curl.setopt(curl.HTTPPOST, form)
             curl.perform()
             curl.close()
