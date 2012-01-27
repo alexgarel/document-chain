14beedfb4a1f4f38e67dea9641f14bb112740128
Alex Garel <alex.garel@gmail.com>
2012-01-27 13:27:53 +0100

    file is no more mandatory for http_submitter

--- src/document_chain/http_submitter.py
+++ src/document_chain/http_submitter.py
@@ -10,13 +10,15 @@ try:
 
         name = 'HTTP file submitter'
 
-        def run_action(self, url, file_path, user=None, password=None,
+        def run_action(self, url, file_path=None, user=None, password=None,
                        field_name='file', **kwargs):
             """Submit file to url
 
             kwargs may contain optionnal parameters to request
             """
-            form = [(field_name, (pycurl.FORM_FILE, file_path))]
+            form = []
+            if file_path is not None:
+                form.append((field_name, (pycurl.FORM_FILE, file_path)))
             form.extend((k, v) for k, v in kwargs.items() if k != 'next')
             curl = pycurl.Curl()
             curl.setopt(pycurl.URL, url)
