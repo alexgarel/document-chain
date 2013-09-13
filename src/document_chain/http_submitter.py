cd1a10fd059350865310d8f15ac13c20deaf0837
Alex Garel <alex.garel@gmail.com>
2013-09-13 17:25:16 +0200

    now supporting custom http headers

--- src/document_chain/http_submitter.py
+++ src/document_chain/http_submitter.py
@@ -1,6 +1,16 @@
-"""this module define a worker to convert document using unoconv
+"""this module define a worker to submit http requests
 
-We abuse a bit unoconv module by monkey patching"""
+You can
+
+- define the url to request
+- define the user and password to use for basic auth
+- a submit file thanks to the file_path parameter
+  associated to field_name
+- add http headers thanks to paramaters beginig with @
+- and add as many request parameters
+"""
+
+
 try:
     import pycurl
 
@@ -10,20 +20,30 @@ try:
 
         name = 'HTTP file submitter'
 
+        HEADER_PREFIX = '@'
+
         def run_action(self, url, file_path=None, user=None, password=None,
                        field_name='file', **kwargs):
             """Submit file to url
 
             kwargs may contain optionnal parameters to request
+            
+            if a kwarg key begins with @ it is taken as an http header
             """
+            headers = [': '.join((k[1:], v)) for k, v in kwargs.items()
+                if k.startswith(self.HEADER_PREFIX)]
             form = []
             if file_path is not None:
                 form.append((field_name, (pycurl.FORM_FILE, file_path)))
-            form.extend((k, v) for k, v in kwargs.items() if k != 'next')
+            form.extend((k, v) for k, v in kwargs.items()
+                if k != 'next' and not k.startswith(self.HEADER_PREFIX))
             curl = pycurl.Curl()
             curl.setopt(pycurl.URL, url)
             if user is not None:
                 curl.setopt(curl.USERPWD, '%s:%s' % (user, password))
+            import pdb;pdb.set_trace()
+            if headers:
+                curl.setopt(pycurl.HTTPHEADER, headers)
             curl.setopt(curl.HTTPPOST, form)
             # FIXME : get output and return code !
             curl.perform()
